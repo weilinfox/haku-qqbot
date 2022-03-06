2db56334ed444be1c10161f6e0aff933a744e1f9
weilinfox <weilin.king@qq.com>
2022-03-06 22:22:29 +0800

    help 命令忘了回车

--- plugins/commands/help.py
+++ plugins/commands/help.py
@@ -1,12 +1,12 @@
 
 def run(message) -> str:
-    return f'常用指令：' \
-           f'music 网易云音乐' \
-           f'qqmusic qq音乐' \
-           f'rss rss订阅' \
-           f'loongnews loongnix.cn首页新闻' \
-           f'debian debian包查询' \
-           f'ubuntu ubuntu包查询' \
-           f'loongnix loongnix20包查询' \
-           f'wyy 网抑云' \
+    return f'常用指令：\n' \
+           f'music 网易云音乐\n' \
+           f'qqmusic qq音乐\n' \
+           f'rss rss订阅\n' \
+           f'loongnews loongnix.cn首页新闻\n' \
+           f'debian debian包查询\n' \
+           f'ubuntu ubuntu包查询\n' \
+           f'loongnix loongnix20包查询\n' \
+           f'wyy 网抑云\n' \
            f'yiyan 一言'
