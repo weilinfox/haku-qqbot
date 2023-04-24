ad0984115d84ee7aaffa43928485f4ec637da5e1
weilinfox <weilin.king@qq.com>
2023-04-24 18:24:45 +0800

    Fix wrong cq code in wttrin

--- plugins/commands/wttrin.py
+++ plugins/commands/wttrin.py
@@ -1,16 +1,15 @@
-import time
 
 from handlers.message import Message
 
 
 def run(message: Message) -> str:
-    helpMsg = '一个访问 wttr.in 的小玩意'
+    help_msg = '一个访问 wttr.in 的小玩意'
     req = list(message.raw_message.split())
     for i in range(0, len(req)):
         req[i] = req[i].strip()
     if i == 0:
-        ans = helpMsg
+        ans = help_msg
     else:
-        ans = '[CQ:image,file=http://wttr.in/' + req[1] + '_tqp0_lang=en.png?hakuflag=' + str(int(time.time())) + ']'
+        ans = '[CQ:image,file=http://wttr.in/' + req[1] + '_tqp0_lang=en.png,cache=0]'
 
     return ans
